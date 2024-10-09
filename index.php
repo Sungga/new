2652f3dfb9c09d68e08f387ef84866b90a2b9f39
Sungga <126399834+Sungga@users.noreply.github.com>
2024-10-09 22:25:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     $serverName = "comp.database.windows.net"; // Tên server, ví dụ: "localhost" hoặc "192.168.1.1"
     $connectionOptions = array(
         "Database" => "sinhvien", // Tên database
-        "Uid" => "thi",           // Tên người dùng
+        "Uid" => "vanh",           // Tên người dùng
         "PWD" => "@A123456"       // Mật khẩu
     );
 
