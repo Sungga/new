3b11198f8598d9d26aa3f856c73fe8e95f898b60
Sungga <126399834+Sungga@users.noreply.github.com>
2024-10-10 14:14:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chat with me</title>
-<!--     <link rel="stylesheet" href="./style.css"> -->
+    <link rel="stylesheet" href="./style.css"> 
 </head>
 <body>
     <div id="main">
