a93c44be59c20b8e3b21df383f95335ee9f75851
Sungga <126399834+Sungga@users.noreply.github.com>
2024-10-27 22:14:04 +0700

    Update send.js

--- send.js
+++ send.js
@@ -24,7 +24,7 @@ function sendMessage() {
                 }, 100);
             }
         };
-        xhr.send("message=" + encodeURIComponent(message) + "&users=" + encodeURIComponent(user));
+        xhr.send("message=" + encodeURIComponent(message) + "&users=" + encodeURIComponent(users));
     } else {
         alert('Please enter a message.');
     }
